688bb5f0ce11f4c717a9d5c57e68d5e2a58e6bbf
Jeff Kelly <acidburner22@hotmail.com>
2017-01-23 15:13:28 -0800

    added player growth logic
    renamed variables in settings.player

--- index.js
+++ index.js
@@ -12,10 +12,16 @@ var app = {
             color: "black",
             //defines players starting cell size (default 5 cells)
             size: 5,
-            location: {
-                x: 14,
+            head: {
+                x: 15,
                 y: 29
-            }
+            },
+            tail: {
+                x: 15,
+                y: 29
+            },
+            growth: 3,
+            direction: "north"
         },
         updateFreq: 1,
         intervalId: 0
@@ -24,20 +30,25 @@ var app = {
     startGame: function(){
         console.log("game started");
         app.drawGrid();
-        app.drawPlayer(app.settings.player.location.x, app.settings.player.location.y);
+        app.drawPlayer(app.settings.player.head.x, app.settings.player.head.y);
         app.settings.intervalId = setInterval( app.updateLoop, ( 1000 / app.settings.updateFreq ) );
     },
     updateLoop: function(){
         console.log("update loop");
         //app.drawPoint();
         //player moves
-        app.movePlayer();
+        app.updatePlayer( app.settings.player.direction );
+        app.checkWallHit( app.settings.player.head.x, app.settings.player.head.y );
         //check point intersect with head
         //if cell has class point and playerHead point was hit
             //increase size if true
             //add point
         //
         //
+    },
+    updatePlayer: function( direction ) {
+        app.movePlayer( direction );
+        app.drawPlayer( app.settings.player.head.x, app.settings.player.head.y );
     },
     checkWallHit: function(x, y) {
         if( x > app.settings.grid.size || x < 0
@@ -49,26 +60,34 @@ var app = {
     movePlayer: function( direction ){
         switch( direction ) {
             case "north":
-                app.settings.player.location.y--;
+                app.settings.player.head.y--;
                 break;
             case "east":
-                app.settings.player.location.x++;
+                app.settings.player.head.x++;
                 break;
             case "south":
-                app.settings.player.location.y++;
+                app.settings.player.head.y++;
                 break;
             case "west":
-                app.settings.player.location.x--;
+                app.settings.player.head.x--;
         }
     },
-    drawPlayer: function(x, y) {
-        $("#x"+x+"-y"+y).addClass("player");
+    drawPlayer: function() {
+        app.drawHead();
+        //if the player growth is more than the length of hte player, remove last item
+        if( $(".player").length > app.settings.player.growth ) {
+            var tail = $(".player").length - 1;
+            $(".player").eq(tail).removeClass("player");
+        }
+    },
+    drawHead: function() {
+        $("#x"+app.settings.player.head.x+"-y"+app.settings.player.head.y).addClass("player").addClass("head");
     },
     drawGrid: function(){
          console.log("draw grid");
          for( var i = 0; i < app.settings.grid.size; i++ ) {
              for( var k = 0; k < app.settings.grid.size; k++ ) {
-                 $("#grid").append("<div class='cells' id='x" + i + "-y" + k + "'></div>");
+                 $("#grid").append("<div class='cells' id='x" + k + "-y" + i + "'></div>");
              }
          }
     },
@@ -76,11 +95,12 @@ var app = {
     drawPoint: function() {
         var x = Math.floor( Math.random() * app.settings.grid.size );
         var y = Math.floor( Math.random() * app.settings.grid.size );
-        console.log("x:" + x, "y:" + y);
-        $("#x"+x+"-y"+y+"").addClass("point");
+        console.log("Drawing point","x:" + x, "y:" + y);
+        $("#x"+x+"-y"+y).addClass("point");
     },
 
     gameOver: function() {
+        console.log("game over");
         clearInterval(app.settings.intervalId);
     }
 }
