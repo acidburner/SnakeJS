16b20f3331768741dbd4c22c34e6c40fff6d835e
Jeff Kelly <acidburner22@hotmail.com>
2017-01-23 16:42:45 -0800

    fixed movement issues
    created a body array to hold the coords of the snake.
    checking the coords of the body array to determine if there's been a
    hit.

--- index.js
+++ index.js
@@ -16,14 +16,11 @@ var app = {
                 x: 15,
                 y: 29
             },
-            tail: {
-                x: 15,
-                y: 29
-            },
+            body: [],
             growth: 3,
             direction: "north"
         },
-        updateFreq: 1,
+        updateFreq: 5,
         intervalId: 0
         
     },
@@ -31,14 +28,35 @@ var app = {
         console.log("game started");
         app.drawGrid();
         app.drawPlayer(app.settings.player.head.x, app.settings.player.head.y);
+        app.setControlEventListener();
         app.settings.intervalId = setInterval( app.updateLoop, ( 1000 / app.settings.updateFreq ) );
+    },
+    setControlEventListener: function() {
+        $(document).on( "keypress", app.setControlEvent );
+    },
+    setControlEvent: function( event ) {
+        console.log(event.key);
+        switch( event.key ) {
+            case "w":
+                app.settings.player.direction = "north";
+                break;
+            case "a":
+                app.settings.player.direction = "west";
+                break;
+            case "s":
+                app.settings.player.direction = "south";
+                break;
+            case "d":
+                app.settings.player.direction = "east";
+                break;
+        }
     },
     updateLoop: function(){
         console.log("update loop");
         //app.drawPoint();
         //player moves
         app.updatePlayer( app.settings.player.direction );
-        app.checkWallHit( app.settings.player.head.x, app.settings.player.head.y );
+        app.checkHit();
         //check point intersect with head
         //if cell has class point and playerHead point was hit
             //increase size if true
@@ -50,38 +68,59 @@ var app = {
         app.movePlayer( direction );
         app.drawPlayer( app.settings.player.head.x, app.settings.player.head.y );
     },
+    checkHit: function(){
+        app.checkWallHit( app.settings.player.head.x, app.settings.player.head.y );
+        app.checkSelfHit();
+    },
     checkWallHit: function(x, y) {
+        //if the player hits a wall, end game
         if( x > app.settings.grid.size || x < 0
          || y > app.settings.grid.size || y < 0
         ) { 
             app.gameOver();
         }
     },
+    checkSelfHit: function() {
+        //if the players head hits its body, end game.
+        for( i = 0; i < app.settings.player.body.length - 1; i++ ) {
+            if( $("#x" + app.settings.player.body[i].x + "-y" + app.settings.player.body[i].y).hasClass("head") ) {
+                app.gameOver();
+            }
+        }
+    },
     movePlayer: function( direction ){
+        console.log(direction);
         switch( direction ) {
             case "north":
-                app.settings.player.head.y--;
+                app.settings.player.head.y -= 1;
                 break;
             case "east":
-                app.settings.player.head.x++;
+                app.settings.player.head.x += 1;
                 break;
             case "south":
-                app.settings.player.head.y++;
+                app.settings.player.head.y += 1;
                 break;
             case "west":
-                app.settings.player.head.x--;
+                app.settings.player.head.x -= 1;
         }
     },
-    drawPlayer: function() {
-        app.drawHead();
+    drawPlayer: function(x, y) {
+        console.log( app.settings.player.body );
+        app.settings.player.body.push( {"x": x, "y": y} );
+        //remove head class
+        $(".player").removeClass("head");
+        //add player class to location
+        $("#x"+x+"-y"+y).addClass("player");
+        console.log("x: " + x, "y: " + y);
+        //add head class to first player item.
+        $("#x"+x+"-y"+y).addClass("head");
         //if the player growth is more than the length of hte player, remove last item
         if( $(".player").length > app.settings.player.growth ) {
-            var tail = $(".player").length - 1;
-            $(".player").eq(tail).removeClass("player");
+            //$("#x"+app.settings.player.tail.x+"-y"+app.settings.player.tail.y).removeClass("player");
+            var body = app.settings.player.body;
+            $("#x"+body[0].x+"-y"+body[0].y).removeClass("player");
+            body.shift();
         }
-    },
-    drawHead: function() {
-        $("#x"+app.settings.player.head.x+"-y"+app.settings.player.head.y).addClass("player").addClass("head");
     },
     drawGrid: function(){
          console.log("draw grid");
